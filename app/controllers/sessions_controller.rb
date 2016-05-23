36118fffe758ac6158d6538b06a85ebeab896ba0
marinacor1 <marinaspam1234@gmail.com>
2016-05-23 16:37:48 -0600

    User model created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
   def create
+    render text: request.env["omniauth.auth"].inspect
+    if user = User.from_omniauth(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+    end
   end
 end
