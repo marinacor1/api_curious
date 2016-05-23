cdf59b72a208e407130fa0e72525bf8dfa901cf4
marinacor1 <marinaspam1234@gmail.com>
2016-05-23 16:52:01 -0600

    Logout functionality created

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "welcome#show"
   get "/auth/github", as: :github_login
   get "/auth/github/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: :logout
 
 end
