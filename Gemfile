b186aefebc2055a4ad9ea19576dc6d04e1eb2673
marinacor1 <marinaspam1234@gmail.com>
2016-05-23 16:43:08 -0600

    Add name to users model

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
+gem 'pry'
 gem 'figaro'
 gem 'omniauth-github'
 # Use postgresql as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(auth_info)
+    where(uid: auth_info[:uid]).first_or_create do |new_user| #take the omniauth authentication info
+      #read the user_id from the auth_info. if user exists with the uid value of that user_id then
+      #we've found the person and can return her
+      #otherwise create them:
+      new_user.uid = auth_info.uid
+      new_user.name = auth_info.extra.raw_info.name
+      new_user.screen_name = auth_info.credentials.token
+      new_user.oauth_token_secret = auth_info.credentials.secret
+    end
+  end
 end
