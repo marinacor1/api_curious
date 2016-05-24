38d83d325d47869340f80eb2016d63313c06f10a
marinacor1 <marinaspam1234@gmail.com>
2016-05-24 16:50:07 -0600

    Add show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,15 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
+      # new_user.image = auth_info.info.image
       new_user.uid = auth_info.uid
-      new_user.name = auth_info.extra.raw_info
-      new_user.screen_name = auth_info.extra.raw_info.screen_name
-      new_user.oauth_token_secret = auth_info.credentials.oauth_token_secret
+      new_user.name = auth_info.info.name
+      # new_user.info = auth_info.extra.raw_info
+      # new_user.blog = auth_info.info.urls["Blog"]
+      new_user.screen_name = auth_info.info.nickname
+      new_user.oauth_token = auth_info.credentials.token
+      # new_user.basic_info = auth_info.extra.raw_info
     end
   end
 
-  # def self.get_user(auth)
-  #   User.where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
-  #     user.nickname = auth[:info][:nickname]
-  #     user.email = auth[:info][:email]
-  #     user.image = auth[:info][:image]
-  #     user.token = auth[:credentials][:token]
-  #     user.save!
-  #   end
-  # end
 end
