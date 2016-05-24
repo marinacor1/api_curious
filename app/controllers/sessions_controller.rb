3dd8b7504584d9be6c678708641434375f2e9dae
marinacor1 <marinaspam1234@gmail.com>
2016-05-24 11:58:28 -0600

    Updated with new user method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.get_user(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to root_path
+
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
     end
@@ -8,6 +12,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.clear
-    redirect_to root_path 
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ActiveRecord::Base
       new_user.oauth_token_secret = auth_info.credentials.oauth_token_secret
     end
   end
+
+  def self.get_user(auth)
+    User.where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
+      user.nickname = auth[:info][:nickname]
+      user.email = auth[:info][:email]
+      user.image = auth[:info][:image]
+      user.token = auth[:credentials][:token]
+      user.save!
+  end
 end
