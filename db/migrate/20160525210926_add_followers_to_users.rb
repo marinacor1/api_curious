5b3463dbba52fc91b2884a458002e5c38989b748
marinacor1 <marinaspam1234@gmail.com>
2016-05-25 15:09:59 -0600

    Add followers and following to users table

---
+++ db/migrate/20160525210926_add_followers_to_users.rb
@@ -0,0 +1,6 @@
+class AddFollowersToUsers < ActiveRecord::Migration
+  def change
+      add_column :users, :followers, :string
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525004742) do
+ActiveRecord::Schema.define(version: 20160525210926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20160525004742) do
     t.datetime "updated_at",         null: false
     t.string   "name"
     t.string   "image"
+    t.string   "following"
+    t.string   "followers"
   end
 
 end
