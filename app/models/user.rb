97e989da8552589c36a9cec306873f64da221351
marinacor1 <marinaspam1234@gmail.com>
2016-05-24 22:37:20 -0600

    Style page for debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
+      binding.pry
       new_user.image = auth_info.info.image
       new_user.uid = auth_info.uid
-      # new_user.name = auth_info.info.name
       new_user.name = auth_info.extra.raw_info.name
       # new_user.info = auth_info.extra.raw_info
       # new_user.blog = auth_info.info.urls["Blog"]
