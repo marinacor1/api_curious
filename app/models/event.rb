b7b897cc8ea3156fb6186a92ecca45443c46a33b
marinacor1 <marinaspam1234@gmail.com>
2016-05-25 19:14:22 -0600

    User feed works

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,10 +5,25 @@ class Event
     @service ||= GithubServices.new(user)
   end
 
+  def type_change(type)
+    if type == "CreateEvent"
+      new_type = "created"
+    elsif type == "WatchEvent"
+      new_type = "starred"
+    elsif type == "ForkEvent"
+      new_type = "forked"
+    elsif type == "MemberEvent"
+      new_type = "added a collaborator to"
+    else
+      type = new_type
+    end
+    new_type
+  end
+
   def all
     temp_events_array = service.events_array
     temp_events_array.map do |event|
-      {name: event[:actor][:login], type: event[:type], repo_name: event[:repo][:name]}
+      {name: event[:actor][:login], type: type_change(event[:type]), repo_name: event[:repo][:name]}
     end
   end
 end
