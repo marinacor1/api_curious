aa90f6d56829f6a5926fa8bf7b151e6636b7e7b4
marinacor1 <marinaspam1234@gmail.com>
2016-05-26 11:27:58 -0600

    Uncommented out spec

--- spec/services/github_services_spec.rb
+++ spec/services/github_services_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
-# xdescribe GithubService do
-#   it "returns all starred repos", :vcr do
-#   service = GithubServices.new
-# end
-# end
+describe GithubService do
+  it "returns all starred repos", :vcr do
+  service = GithubServices.new
+  starred = service.starred 
+  expect(starred.count).to eq(3)
+end
+end
